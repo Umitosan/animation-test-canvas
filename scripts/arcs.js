06f90e02578785996a54a1636ec334ad60dfeacb
Dominic Brown <yeahdom@gmail.com>
2018-04-11 22:29:30 -0700

    add magnification to arc anim

--- scripts/arcs.js
+++ scripts/arcs.js
@@ -10,6 +10,8 @@ var canvas3 = $('#canvas3')[0],
 function Arc(x,y,r,color,context) {
   this.x = x;
   this.y = y;
+  this.originalR = r;
+  this.rVel = undefined; // rate of Radius change
   this.r = r;
   this.lineW = 2;
   this.color = color;
@@ -18,6 +20,25 @@ function Arc(x,y,r,color,context) {
   this.eAngle = 2 * Math.PI;
   this.xVel = getRandomIntInclusive(1,2)*randSign(); // rand speed and direction
   this.yVel = getRandomIntInclusive(1,2)*randSign(); // rand speed and direction
+
+  this.init = function() {
+    let randRVel = getRandomIntInclusive(1,2)/4;
+    if (r < 10) {
+      this.rVel = randRVel;
+    } else if (r > 22) {
+      this.rVel = -randRVel;
+    } else {
+      this.rVel = randRVel*randSign();
+    }
+  };
+
+  this.changeRad = function() {
+    if ( ((this.r + this.rVel) > 40) || ((this.r + this.rVel) < 2) ) {
+      this.rVel *= -1;
+    } else {
+      this.r += this.rVel;
+    }
+  };
 
   this.draw = function() {
     // context.arc(x,y,r,sAngle,eAngle,counterclockwise);
@@ -34,24 +55,29 @@ function Arc(x,y,r,color,context) {
   }; // draw
 
   this.update = function() {
+    // check bounds
     if (  ((this.x + this.xVel + this.r + this.lineW) > canvas3.width) || ((this.x + this.xVel - this.r - this.lineW) < 0)  ) {
       this.xVel *= -1;
     }
     if (  ((this.y + this.yVel + this.r) > canvas3.height) || ((this.y + this.yVel - this.r) < 0)  ) {
       this.yVel *= -1;
     }
+    // change vel
     this.x += this.xVel;
     this.y += this.yVel;
+    // change radius
+    this.changeRad();
   }; // update
 } // Arc
 
 function ArcGroup(quantity, context) {
   this.arcs = [];
   this.context = context;
+  this.quantity = quantity;
 
   this.init = function() {
-    for (let i = 0; i < quantity; i++) {
-      let randRad = getRandomIntInclusive(2, 32);
+    for (let i = 0; i < this.quantity; i++) {
+      let randRad = getRandomIntInclusive(2, 40);
       //  arc(x,y,radius,startAngle,endAngle,);
       this.arcs.push( new Arc(getRandomIntInclusive(randRad, canvas3.width-randRad),
                               getRandomIntInclusive(randRad, canvas3.height-randRad),
@@ -59,6 +85,9 @@ function ArcGroup(quantity, context) {
                               randColor('rgba'),
                               this.context
                      ));
+    }
+    for (let j = 0; j < this.arcs.length; j++) {
+      this.arcs[j].init();
     }
   };
 
